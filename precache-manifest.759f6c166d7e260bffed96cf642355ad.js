a3e06f83bcb02589f81ac09cf92cfffbd991849b
mogimog <hamzaotmanivfx@gmail.com>
2020-04-15 19:31:36 +0200

    Updates

---
+++ precache-manifest.759f6c166d7e260bffed96cf642355ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ccc363a47e86621795db08fed754905",
+    "url": "/quiz-test-app/index.html"
+  },
+  {
+    "revision": "e6765863953099fcbb04",
+    "url": "/quiz-test-app/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "86dd2432491c9c4303e1",
+    "url": "/quiz-test-app/static/js/2.1b8e2c2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-test-app/static/js/2.1b8e2c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6765863953099fcbb04",
+    "url": "/quiz-test-app/static/js/main.af043e51.chunk.js"
+  },
+  {
+    "revision": "9f04d276f84f61de42fd",
+    "url": "/quiz-test-app/static/js/runtime-main.2cd6c03c.js"
+  }
+]);
